7f740ac97d50a1fea53ea31741965584a224a0d2
Iylin <kiylin94@naver.com>
2020-10-03 15:22:33 +0900

    Updates

---
+++ precache-manifest.778b9419672e31261169dacd74cae40f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392d2be0be87a11a914ad8442fc19c3f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "2304b725b931acb15c6a",
+    "url": "/movie_app_2020/static/css/main.756e0f15.chunk.css"
+  },
+  {
+    "revision": "2a035e8132460d55a7f8",
+    "url": "/movie_app_2020/static/js/2.cff13977.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.cff13977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2304b725b931acb15c6a",
+    "url": "/movie_app_2020/static/js/main.9c9b3128.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
